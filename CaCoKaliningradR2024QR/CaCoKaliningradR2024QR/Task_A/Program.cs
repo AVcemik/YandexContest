9d40ad97c13115c186678fedac0f330f094f4d0c
cemik <aveal.cemik@gmail.com>
2024-09-16 18:11:12 +0200

    Ready Task_A

---
+++ CaCoKaliningradR2024QR/CaCoKaliningradR2024QR/Task_A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()!);
+            long result = 1;
+
+            if (N >= 1000003) { result = 0; }
+            else
+            {
+                for (long i = 1; i <= N; i++)
+                {
+                    result *= i;
+                    result %= 1000003;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
